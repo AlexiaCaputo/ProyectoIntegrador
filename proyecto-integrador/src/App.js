4bc746a89092c393167fd04159c915696daf4cc2
AlexiaCaputo <caputoa@udesa.edu.ar>
2026-03-27 16:25:49 -0300

    navbar

--- proyecto-integrador/src/App.js
+++ proyecto-integrador/src/App.js
@@ -7,6 +7,7 @@ import Detalle from './screen/Detalle/Detalle';
 import Fav from './screen/Favoritos/Favoritos';
 import Resultados from './screen/Resultados/Resultados';
 import NotFound from './screen/NotFound/NotFound';
+import Crear from './screen/CrearCuenta/CrearCuenta';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/login" component={Login} />
+        <Route path="/register" component={Crear} />
         <Route path="/peliculas" component={Peli} />
         <Route path="/pelicula/:id" component={Detalle} />
         <Route path="/favoritos" component={Fav} />
--- proyecto-integrador/src/components/Navbar/Navbar.js
+++ proyecto-integrador/src/components/Navbar/Navbar.js
@@ -1,5 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 function Navbar() {
-  return <h1>Navbar</h1>;
+
+  let haySesion = localStorage.getItem("login");
+
+  return (
+    <nav>
+      <h2>Nombre y logo</h2>
+      <Link to="/">Home</Link>
+
+      {haySesion ? (
+        <>
+          <Link to="/peliculas">Todas las películas</Link>
+          <Link to="/favoritos">Películas favoritas</Link>
+        </>
+      ) : (
+        <>
+          <Link to="/login">Intentelo de nuevo</Link>
+          <Link to="/register">Crear Cuenta</Link>
+        </>
+      )}
+
+    </nav>
+  );
 }
 
 export default Navbar;
