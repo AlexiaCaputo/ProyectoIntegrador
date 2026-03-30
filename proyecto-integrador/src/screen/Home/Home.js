76db3d4be484435cf2e8412d81327e1a7d2ea9c0
TDietrichOxenford <tdietrichoxenford@udesa.edu.ar>
2026-03-29 21:29:05 -0300

    avance el home, vincule las apis

--- proyecto-integrador/src/screen/Home/Home.js
+++ proyecto-integrador/src/screen/Home/Home.js
@@ -1,11 +1,30 @@
 import React, { Component } from "react";
 
 class Home extends Component {
+  
   constructor(props) {
     super(props);
     this.state = {
-      busqueda: ""
+      busqueda: "",
+      populares: [],
+      cartel: []
     };
+  }
+
+  componentDidMount() {
+    fetch("https://api.themoviedb.org/3/movie/popular?api_key=fa048358caf9c6d86d3611e5961e0b6d")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({populares: data.results});
+      })
+      .catch((error) => console.log(error));
+  
+    fetch("https://api.themoviedb.org/3/movie/now_playing?api_key=fa048358caf9c6d86d3611e5961e0b6d")
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({cartel: data.results});
+      })
+      .catch((error) => console.log(error));
   }
 
   evitarSubmit(event) {
@@ -21,7 +40,7 @@ class Home extends Component {
   render() {
     return (
       <main>
-        <form onSubmit={(event) => this.evitarSubmit(event)}>
+        <form className="input-busqueda" onSubmit={(event) => this.evitarSubmit(event)}>
           <input
             type="text"
             value={this.state.busqueda}
@@ -31,11 +50,27 @@ class Home extends Component {
         </form>
 
         <section>
-          <h2>Películas populares</h2>
+          <h2>Películas más populares</h2>
+          <div className="card-container">
+          {this.state.populares.slice(0, 4).map((pelicula) => (
+            <article className="character-card" key={pelicula.id}>
+              <img src={`https://image.tmdb.org/t/p/w342${pelicula.poster_path}`} alt={pelicula.title} />
+              <h3>{pelicula.title}</h3>
+            </article>
+          ))}
+          </div>
         </section>
 
         <section>
           <h2>Películas en cartel</h2>
+          <div className="card-container">
+          {this.state.cartel.slice(0, 4).map((pelicula) => (
+            <article className="character-card" key={pelicula.id}>
+              <img src={`https://image.tmdb.org/t/p/w342${pelicula.poster_path}`} alt={pelicula.title} />
+              <h3>{pelicula.title}</h3>
+            </article>
+          ))}
+          </div>
         </section>
       </main>
     );
